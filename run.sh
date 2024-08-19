4dfda6102fed567ff4e989cd8b4da8d571ef6fe0
Artem Matiashov <matiashov.artem@gmail.com>
2024-08-19 20:05:37 +0300

    Use bcrypt hash password

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 clear
+apt install -y apache2-utils
+
 # https://stackoverflow.com/a/66946735
 export WG_HOST=$(curl ifconfig.me)
-export WG_PASSWORD=$(openssl rand -base64 15)
+export RAW_WG_PASSWORD=$(openssl rand -base64 15)
+# https://unix.stackexchange.com/a/419855
+export PASSWORD_HASH=$(htpasswd -bnBC 10 "" ${RAW_WG_PASSWORD} | tr -d ':\n')
+
 docker compose up -d
+
 echo "${WG_HOST}:51821"
-echo ${WG_PASSWORD}
+echo ${RAW_WG_PASSWORD}
